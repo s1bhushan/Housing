480eef4944520d2a719c20920069b9543f6641a3
Bhushan Sonawane <bsonawane990@gmail.com>
2025-04-19 13:01:24 +0530

    checking ignore

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,127 +1,127 @@
 
-import { useState } from 'react';
-import axios from 'axios';
+// import { useState } from 'react';
+// import axios from 'axios';
 
 
 
-export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [msg, setMsg] = useState('');
+// export default function Login() {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [msg, setMsg] = useState('');
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post('http://localhost:8081/api/auth/login', { email, password });
-      localStorage.setItem('token', res.data.token);
-      setMsg("Login successful!");
-    } catch (err) {
-      setMsg("Invalid credentials");
-    }
-  };
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const res = await axios.post('http://localhost:8081/api/auth/login', { email, password });
+//       localStorage.setItem('token', res.data.token);
+//       setMsg("Login successful!");
+//     } catch (err) {
+//       setMsg("Invalid credentials");
+//     }
+//   };
 
 
 
-    return(
+//     return(
 
-        <div>
+//         <div>
 
-<div className="blackfieldfornav">
+// <div className="blackfieldfornav">
 
-</div>
+// </div>
 
-<section class="vh-100">
-  <div class="container-fluid h-custom">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          class="img-fluid" alt="Sampleimage"/>
-      </div>
-      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <form onSubmit={handleLogin}>
-          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-            <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
-              <i class="fab fa-facebook-f"></i>
-            </button>
+// <section class="vh-100">
+//   <div class="container-fluid h-custom">
+//     <div class="row d-flex justify-content-center align-items-center h-100">
+//       <div class="col-md-9 col-lg-6 col-xl-5">
+//         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+//           class="img-fluid" alt="Sampleimage"/>
+//       </div>
+//       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+//         <form onSubmit={handleLogin}>
+//           <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+//             <p class="lead fw-normal mb-0 me-3">Sign in with</p>
+//             <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
+//               <i class="fab fa-facebook-f"></i>
+//             </button>
 
-            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
-              <i class="fab fa-twitter"></i>
-            </button>
+//             <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
+//               <i class="fab fa-twitter"></i>
+//             </button>
 
-            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
-              <i class="fab fa-linkedin-in"></i>
-            </button>
-          </div>
+//             <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
+//               <i class="fab fa-linkedin-in"></i>
+//             </button>
+//           </div>
 
-          <div class="divider d-flex align-items-center my-4">
-            <p class="text-center fw-bold mx-3 mb-0">Or</p>
-          </div>
+//           <div class="divider d-flex align-items-center my-4">
+//             <p class="text-center fw-bold mx-3 mb-0">Or</p>
+//           </div>
 
-          {/* <!-- Email input --> */}
-          <div data-mdb-input-init class="form-outline mb-4">
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="form3Example3" class="form-control form-control-lg"
-              placeholder="Enter a valid email address" />
-            <label class="form-label" for="form3Example3">Email address</label>
-          </div>
+//           {/* <!-- Email input --> */}
+//           <div data-mdb-input-init class="form-outline mb-4">
+//             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="form3Example3" class="form-control form-control-lg"
+//               placeholder="Enter a valid email address" />
+//             <label class="form-label" for="form3Example3">Email address</label>
+//           </div>
 
-          {/* <!-- Password input --> */}
-          <div data-mdb-input-init class="form-outline mb-3">
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}  id="form3Example4" class="form-control form-control-lg"
-              placeholder="Enter password" />
-            <label class="form-label" for="form3Example4">Password</label>
-          </div>
+//           {/* <!-- Password input --> */}
+//           <div data-mdb-input-init class="form-outline mb-3">
+//             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}  id="form3Example4" class="form-control form-control-lg"
+//               placeholder="Enter password" />
+//             <label class="form-label" for="form3Example4">Password</label>
+//           </div>
 
-          <div class="d-flex justify-content-between align-items-center">
-            {/* <!-- Checkbox --> */}
-            <div class="form-check mb-0">
-              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-              <label class="form-check-label" for="form2Example3">
-                Remember me
-              </label>
-            </div>
-            <a href="#!" class="text-body">Forgot password?</a>
-          </div>
+//           <div class="d-flex justify-content-between align-items-center">
+//             {/* <!-- Checkbox --> */}
+//             <div class="form-check mb-0">
+//               <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+//               <label class="form-check-label" for="form2Example3">
+//                 Remember me
+//               </label>
+//             </div>
+//             <a href="#!" class="text-body">Forgot password?</a>
+//           </div>
 
-          <div class="text-center text-lg-start mt-4 pt-2">
-            <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
-              >Login</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/register"
-                class="link-danger">Register</a></p>
-          </div>
+//           <div class="text-center text-lg-start mt-4 pt-2">
+//             <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+//               >Login</button>
+//             <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/register"
+//                 class="link-danger">Register</a></p>
+//           </div>
 
-        </form>
-        <p>{msg}</p>
-      </div>
-    </div>
-  </div>
-  <div
-    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-    {/* <!-- Copyright --> */}
-    <div class="text-white mb-3 mb-md-0">
-      Copyright © 2020. All rights reserved.
-    </div>
-    {/* <!-- Copyright --> */}
+//         </form>
+//         <p>{msg}</p>
+//       </div>
+//     </div>
+//   </div>
+//   <div
+//     class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+//     {/* <!-- Copyright --> */}
+//     <div class="text-white mb-3 mb-md-0">
+//       Copyright © 2020. All rights reserved.
+//     </div>
+//     {/* <!-- Copyright --> */}
 
-    {/* <!-- Right --> */}
-    <div>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="#!" class="text-white">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-    </div>
-    {/* <!-- Right --> */}
-  </div>
-</section>
+//     {/* <!-- Right --> */}
+//     <div>
+//       <a href="#!" class="text-white me-4">
+//         <i class="fab fa-facebook-f"></i>
+//       </a>
+//       <a href="#!" class="text-white me-4">
+//         <i class="fab fa-twitter"></i>
+//       </a>
+//       <a href="#!" class="text-white me-4">
+//         <i class="fab fa-google"></i>
+//       </a>
+//       <a href="#!" class="text-white">
+//         <i class="fab fa-linkedin-in"></i>
+//       </a>
+//     </div>
+//     {/* <!-- Right --> */}
+//   </div>
+// </section>
 
-      </div>
-    );
-}
+//       </div>
+//     );
+// }
